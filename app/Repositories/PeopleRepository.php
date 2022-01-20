c1e75a3dff3d83c10219e192d609fbfdf34cf929
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2022-01-20 12:30:01 -0300

    [ADD] - SOLID principles. Parte 14: get data people.

--- app/Repositories/PeopleRepository.php
+++ app/Repositories/PeopleRepository.php
@@ -3,20 +3,31 @@
 namespace App\Repositories;
 
 use App\Models\People;
+use App\Models\Physic;
 
 class PeopleRepository
 {
     protected $people;
+    protected $physic;
 
-    public function __construct(People $people)
+    public function __construct(
+        People $people,
+        Physic $physic
+    )
     {
         $this->people = $people;
+        $this->physic = $physic;
     }
 
     public function getAllPeople()
     {
-        return $this->people
-            ->all();
+        return $this->physic
+            ->query()
+            ->select('first_name','last_name','cpf','city', 'district', 'uf', 'county', 'zip_code', 'phone_number')
+            ->join('peoples', 'peoples.id', '=', 'people_id')
+            ->join('contacts', 'contacts.people_id', '=', 'id')
+            ->join('addresses', 'addresses.people_id', '=', 'id')
+            ->get();
     }
 
     public function getById($id)
