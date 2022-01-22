7f315f988c1cbfc5122ce5682b2573f5b03224e1
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2022-01-22 18:01:01 -0300

    [ADD] - SOLID principles. Parte 21: refactoring

--- app/Services/PersonService.php
+++ app/Services/PersonService.php
@@ -2,15 +2,21 @@
 
 namespace App\Services;
 
+use App\Ctos\PersonCto;
 use App\Repositories\PersonRepository;
 
 class PersonService
 {
     protected $personRepository;
+    protected $personCto;
 
-    public function __construct(PersonRepository $personRepository)
+    public function __construct(
+        PersonRepository $personRepository,
+        PersonCto $personCto
+    )
     {
         $this->personRepository = $personRepository;
+        $this->personCto = $personCto;
     }
 
     public function getAll()
@@ -21,5 +27,10 @@ class PersonService
     public function save($request)
     {
         return $this->personRepository->save($request);
+    }
+
+    public function register($request)
+    {
+        return $this->personCto->save($request);
     }
 }
